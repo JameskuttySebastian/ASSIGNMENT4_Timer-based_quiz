9186e5a2d3e18d6b319fc5cfa1becd3c02e01132
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2019-11-18 23:33:34 +1030

    created psudo code for js file and created html page

---
+++ Assets/script.js
@@ -0,0 +1,48 @@
+//define arrays for questions
+//order as [question, option_1,option_2, option_3, option_4, Ans]
+
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+];
+
+//variable to save correct answer count, remaining time
+var timeTaken = 0;
+
+//start quiz loop through the questions
+
+//calculate score (in time) based on 1. answer
+
+//wrong answer, 15 seconds added
+
+
+//2. right answer and time took to answer
+// within 5 sec, 10 points
+//within 10 sec, 5 points
+//more than 10 seconds, 1 point
+
+//final score and save into local storage
+
