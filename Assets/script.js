608332b58b2bb0dbd64e66edaaf1795bfbaaf850
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2019-11-20 22:57:46 +1030

    removed the calculation of score

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,16 @@
 //define arrays for questions
 //order as [question, option_1,option_2, option_3, option_4, Ans]
 var startBtn = document.querySelector("#startBtn");
+var viewHighScore = document.querySelector("#viewHighScore");
 var ansOptions = document.querySelector("#ansOptions");
 var question = document.querySelector("#question");
 var result = document.querySelector("#result");
+// var totalScore = document.querySelector("#totalScore");
 var timer = document.querySelector("#timer");
+var warning = document.querySelector("#warning");
+
+
+
 var questions = [
     {
         title: "Commonly used data types DO NOT include:",
@@ -37,20 +43,37 @@ var questions = [
 // var timeTaken = 0;
 var timeTakenForQuestion = 0;
 var questionIndex = 0;
-var timeTaken = 0;
-var score = 0;
+var timeTaken = 75;
+// var score = 0;
 
 
 var timeElapsed = "";
+result.parentElement.style.display ="none";
+warning.style.display ="none";
 
 function startTimer() {
     timeElapsed = setInterval(function () {
-        timeTaken = timeTaken + 1;
+        timeTaken = timeTaken - 1;
         timer.textContent = timeTaken;
+        if(timeTaken<=0){
+            clearQuestion()
+            timeTaken = 0;
+            timer.textContent = timeTaken;
+            
+            finalScore() ;
+            warning.style.display ="block";
+            warning.innerHTML = "The time is over!.......";
+            clearInterval(timeElapsed);
+        }
     }, 1000);
 }
 //start quiz loop through the questions
 startBtn.addEventListener("click", function (event) {
+    startBtn.style.display ="none";
+    viewHighScore.style.display ="none";
+    timeTaken = 75;
+    // score = 0;
+    // totalScore.textContent = score;
     startTimer();
     displayQuestion();
 });
@@ -72,8 +95,13 @@ function displayQuestion() {
 }
 
 function finalScore() {
-    console.log("finished");
-    questionIndex = 0;
+    // console.log("finished");
+
+    //changing button caption
+    startBtn.style.display ="block";
+    startBtn.textContent = "Go Back to Quiz";
+    viewHighScore.style.display ="block";
+    questionIndex = 0;    
 }
 
 function nextQuestion() {
@@ -100,30 +128,34 @@ function verifyUserAnswer(event) {
     var userAnswer = event.target.parentElement;
     // console.log("target clicked = ", userAnswer);
     var indexOfAnswer = parseInt(userAnswer.getAttribute("id"));
-    console.log("User choose : " + indexOfAnswer);
+    // console.log("User choose : " + indexOfAnswer);
 
 
     var answerString = questions[questionIndex].answer;
 
     var ansArray = questions[questionIndex].choices;
     var ansIndux = parseInt(ansArray.indexOf(answerString));
-    console.log("CorrectAns is : " + ansIndux);
+    // console.log("CorrectAns is : " + ansIndux);
     var resultOfQuestion = "";
     if (indexOfAnswer === ansIndux) {
         // console.log(indexOfAnswer + " : index: " + ansIndux + " :correct");
-        resultOfQuestion = "Correct Answer"
-
+        resultOfQuestion = "correct"
+        // score = score + 10;
     }
     else {
         // console.log(indexOfAnswer + " : index: " + ansIndux + " :wrong");
-        resultOfQuestion = "Wrong Answer"
-        timeTaken = timeTaken + 15;
+        resultOfQuestion = "wrong"
+        timeTaken = timeTaken - 15;
+        // score = score - 5;
     }
     //show answer for two seconds
+    result.parentElement.style.display ="block";
     result.textContent = resultOfQuestion;
+    // totalScore.textContent = score;
 
     var secondsDelay2 = setInterval(function () {
         clearInterval(secondsDelay2);
+        result.parentElement.style.display ="none";
         clearQuestion();
         nextQuestion();
     }, 2000);
@@ -138,18 +170,3 @@ ansOptions.addEventListener("click", function (event) {
         verifyUserAnswer(event);
     }
 });
-
-
-
-//calculate score (in time) based on 1. answer
-
-//wrong answer, 15 seconds added
-
-
-//2. right answer and time took to answer
-// within 5 sec, 10 points
-//within 10 sec, 5 points
-//more than 10 seconds, 1 point
-
-//final score and save into local storage
-
